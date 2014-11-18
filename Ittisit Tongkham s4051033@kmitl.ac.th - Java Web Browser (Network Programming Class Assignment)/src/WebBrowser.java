b2ef97afdb99f269b0a02e321e57cba9b65bef33
54051033 <I-Boss@boss>
2014-11-19 00:05:31 +0700

    Init

---
+++ Ittisit Tongkham s4051033@kmitl.ac.th - Java Web Browser (Network Programming Class Assignment)/src/WebBrowser.java
@@ -0,0 +1,24 @@
+
+import javax.swing.JFrame;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author I-Boss
+ */
+public class WebBrowser {
+    
+    public static void main(String [] args)
+	{
+		JFrame frame = new NewJFrame();
+		frame.show();
+	}
+    
+    
+}
+
